049447edf9e8682a3cb04f1dc852fb123bf953ac
Frank <frank.mendez.contractor@nomosone.com>
2022-06-14 13:33:34 +0800

    CDK Project exploration

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,10 +1,13 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    new Bucket(this, 'someBucket', {lifecycleRules: [{expiration: Duration.days(5)}]})
 
     // The code that defines your stack goes here
 
